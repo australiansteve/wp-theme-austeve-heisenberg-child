d7e5ca53cb886a3cbed3f2ae1c7984a4ee3a6619
Steve <steve@australiansteve.com>
2017-12-18 23:07:14 -0400

    Add support for custom theme logo. enqueue style from gulp task output rather than default style.css

--- functions.php
+++ functions.php
@@ -7,10 +7,28 @@ add_action( 'wp_enqueue_scripts', function() {
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/_dist/css/app.css' );
     wp_enqueue_style( $parent_style.'-login', get_template_directory_uri() . '/_dist/css/login.min.css' );
     wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
+        get_stylesheet_directory_uri() . '/_dist/css/app.css',
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
 });
+
+
+add_action( 'after_setup_theme', function() {
+
+	/*
+	 * Enable supoprt for custom logo
+	 * See https://codex.wordpress.org/Theme_Logo
+	 */
+	add_theme_support( 'custom-logo', array(
+		'height'      => 250,
+		'width'       => 250,
+		'flex-height' => false,
+		'flex-width'  => true,
+		'header-text' => array( 'site-title', 'site-description' ),
+	) );
+
+});
+
 
 ?>

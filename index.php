d15ac9ce4bc65856eb4269c2bc94709dee3420ff
Steve <steve@australiansteve.com>
2018-02-06 23:43:00 -0400

    Index page changes to width of content on large screen

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+get_header(); ?>
+
+	<div class="row">
+
+		<main class="medium-8 large-6 columns">
+			<?php
+			if ( have_posts() ) :
+
+				while ( have_posts() ) :
+
+					the_post();
+
+					the_title( '<h1>', '</h1>' );
+
+					the_content();
+
+				endwhile;
+
+				the_posts_navigation();
+
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			else :
+
+				echo esc_html( 'Sorry, no posts' );
+
+			endif; ?>
+
+		</main>
+
+		<aside class="medium-4 large-6 columns">
+
+			<?php get_sidebar(); ?>
+
+		</aside>
+
+	</div>
+
+<?php
+get_footer();

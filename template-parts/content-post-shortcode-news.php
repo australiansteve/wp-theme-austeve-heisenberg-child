c09d8e8e543b65580069f4e37b0522bb9046e071
Steve <steve@australiansteve.com>
2018-03-11 11:04:31 -0300

    Display title by getting the post first, as the_title doesn't work in secondary loop like this

--- template-parts/content-post-shortcode-news.php
+++ template-parts/content-post-shortcode-news.php
@@ -3,7 +3,7 @@
 <div class="post-block" data-post='<?php echo get_the_ID(); ?>'>
 <div class="grid-y container" style="height: 20rem;">
 	<div class="cell shrink title">
-		<?php the_title( '<h1>', '</h1>' ); ?>
+		 <h1><?php echo get_post()->post_title; ?></h1>
 	</div>
 	<div class="cell auto cell-block-container excerpt">
 		<?php the_excerpt(); ?>

830829186789c72aaa1094538ec34b5f5f158daf
Steve <steve@australiansteve.com>
2018-03-12 22:05:12 -0300

    Shrink logo when user scrolls down from top of page

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,40 @@ function diagonalAngle()
 	return Math.floor((Math.random() * 360) + 1) - 180;
 }
 
+// extension:
+jQuery.fn.scrollEnd = function(callback, timeout) {          
+  jQuery(this).scroll(function(){
+    var $this = jQuery(this);
+    if ($this.data('scrollTimeout')) {
+      clearTimeout($this.data('scrollTimeout'));
+    }
+    $this.data('scrollTimeout', setTimeout(callback,timeout));
+  });
+};
+
+// how to call it (with a 1000ms timeout):
+jQuery(window).scrollEnd(function(){
+    if (jQuery(this).scrollTop() > 0)
+    {
+    	//transition logo smaller
+    	jQuery("header .custom-logo-link img").each(function() {
+			jQuery(this).css("max-width", "100px");
+			jQuery(this).css("-webkit-transition", "max-width .5s ease");
+			jQuery(this).css("transition", "max-width .5s ease");
+		});
+    	
+    }
+    else 
+    {
+    	//transition logo bigger
+    	jQuery("header .custom-logo-link img").each(function() {
+			jQuery(this).css("max-width", "180px");
+			jQuery(this).css("-webkit-transition", "max-width .5s ease");
+			jQuery(this).css("transition", "max-width .5s ease");
+		});
+    }
+}, 1000);
+
 jQuery( document ).ready(function() {
     jQuery(".post .post-block").each(function(){
 	  var angle = diagonalAngle();
@@ -24,6 +58,7 @@ jQuery( document ).ready(function() {
 	});
 
 });
+
 
 
 

8c17e6ae4edbf6ed5ded93daa7e8f63ec746ad1a
Steve <steve@australiansteve.com>
2018-07-30 21:40:15 -0300

    Basic footer styling minus fonts

--- footer.php
+++ footer.php
@@ -16,7 +16,19 @@
 				<div class="column row">
 
 					<p class="text-center">
-						<a href="https://weavercrawford.com">&copy; <?php echo date("Y"); ?> Weaver Crawford Creative</a>
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/_dist/img/ecb-logo-footer.png" />
+					</p>
+
+					<p class="footer-address">
+						60 Prince William St, Saint John, NB E2L 2B1
+					</p>
+					
+					<p class="footer-signup">
+						<a href="mailchimp.com" target="_blank">sign up for our newsletter</a>
+					</p>
+
+					<p class="footer-social">
+						<a href="https://instagram.com" target="_blank"><i class="fab fa-instagram fa-2x"></i></a><a href="https://facebook.com" target="_blank"><i class="fab fa-facebook fa-2x"></i></a>
 					</p>
 
 				</div><!-- .column.row -->

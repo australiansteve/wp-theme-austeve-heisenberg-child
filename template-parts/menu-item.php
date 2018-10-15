38f6b190133fab054a833993479bb0d1defac15a
Steve <steve@australiansteve.com>
2018-10-15 20:55:16 -0300

    Complete styling of menu items prices and descriptions

--- template-parts/menu-item.php
+++ template-parts/menu-item.php
@@ -8,10 +8,10 @@
 				<?php echo get_the_title( ); ?>
 			</h2>
 		</div>
-		<div class="cell small-3">
-			<h2 class='price'>
+		<div class="cell small-3 price">
+			<p class='price'>
 				<?php echo get_field('price');?>
-			</h2>
+			</p>
 		</div>
 	</div>
 	<div class='grid-x grid-padding-x'>

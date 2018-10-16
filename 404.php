38948c0c9261ca2e55c55927390d4ceecc076b83
Steve <steve@australiansteve.com>
2018-10-15 22:47:50 -0300

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,21 @@
+<?php
+get_header(); ?>
+
+	<div class="row">
+
+		<main class="small-12 columns lost">
+			
+			<h1>Looking for something?</h1>
+
+			<p>The page you're looking for doesn't exist</p>
+
+			<p><a href="<?php echo site_url();?>">Take me home</a></p>
+
+
+
+		</main>
+
+	</div>
+
+<?php
+get_footer();

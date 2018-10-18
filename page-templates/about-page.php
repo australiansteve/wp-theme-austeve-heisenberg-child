10a42b44175dbac3b45ad622dd4f3f3dfea7aee0
Steve <steve@australiansteve.com>
2018-10-18 13:28:51 -0300

    Put portrait pictures on about page and include page content. Style to completion

--- page-templates/about-page.php
+++ page-templates/about-page.php
@@ -22,9 +22,21 @@ get_header(); ?>
 
 	<div class="row">
 	
-		<div class="small-12 columns austeve-about-title">
+		<div class="small-12 columns austeve-about-title text-center ">
 
 			<?php echo the_title('<h1>', '</h1>') ?>
+			
+		</div>
+
+	</div>
+
+	<div class="row">
+	
+		<div class="small-12 columns austeve-about-content text-center">
+
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<?php the_content(); ?>
+			<?php endwhile; endif; ?>
 			
 		</div>
 
--- template-parts/team-member.php
+++ template-parts/team-member.php
@@ -7,19 +7,19 @@
 	<div class="small-12 medium-6 large-4 columns">
 
 			<?php if (has_post_thumbnail( $post->ID ) ): ?>
-				<?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'medium-square'); ?>
-				<img src="<?php echo $image[0];?>" width="400px" height="400px"/>
+				<?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'medium-portrait'); ?>
+				<img src="<?php echo $image[0];?>" width="400px" height="600px"/>
 			<?php endif; ?>
 
 	</div>
 
 	<div class="small-12 medium-6 large-8 columns text-center">
 
-		<h2 class='title'>
+		<h2 class='title medium-text-left'>
 			<?php echo get_the_title( ); ?>
 		</h2>
 
-		<p class='description'>
+		<p class='description medium-text-left'>
 			<?php echo get_field('description'); ?>
 		</p>
 

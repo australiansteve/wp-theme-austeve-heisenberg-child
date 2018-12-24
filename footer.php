172a337bf373367a10355126644f76bcf9b7772d
Steve <steve@australiansteve.com>
2018-12-23 21:00:26 -0500

    Basic footer styling

--- footer.php
+++ footer.php
@@ -21,91 +21,100 @@
 
 		<div class="cell small-12 medium-4 left-column">
 
-			<strong>Street address</strong>
-			<p class="street-address">
-				<?php the_field('street_address', 'option'); ?>
-			</p>
+			<div class="container">
+				<strong>Street address</strong>
+				<p class="street-address">
+					<?php the_field('street_address', 'option'); ?>
+				</p>
+			</div>
 
-
-			<strong>Mailing address</strong>
-			<p class="mailing-address">
-				<?php the_field('mailing_address', 'option'); ?>
-			</p>
+			<div class="container">
+				<strong>Mailing address</strong>
+				<p class="mailing-address">
+					<?php the_field('mailing_address', 'option'); ?>
+				</p>
+			</div>
 
 		</div>
 
 		<div class="cell small-12 medium-4 center-column">
 
-			<strong>Contact</strong>
-			<p class="contact">
-				<?php the_field('contact', 'option'); ?>
-			</p>
+			<div class="container">
+				<strong>Contact</strong>
+				<p class="contact">
+					<?php the_field('contact', 'option'); ?>
+				</p>
+			</div>
 
 
-			<strong>Find us on</strong>
-			<p class="find-us-on">
-				<?php 
-				$findUsOn = get_field('find_us_on', 'option'); 
-				if( have_rows('find_us_on', 'option') ):
+			<div class="container">
+				<strong>Find us on</strong>
+				<p class="find-us-on">
+					<?php 
+					$findUsOn = get_field('find_us_on', 'option'); 
+					if( have_rows('find_us_on', 'option') ):
 
-				 	// loop through the rows of data
-				    while ( have_rows('find_us_on', 'option') ) : the_row();
+					 	// loop through the rows of data
+					    while ( have_rows('find_us_on', 'option') ) : the_row();
 
-				        // display a sub field value
-				        $url = get_sub_field('footer_find_us_link', 'option');
+					        // display a sub field value
+					        $url = get_sub_field('footer_find_us_link', 'option');
 
-				        if ($url)
-				        {
-				        	echo "<a href='".$url."' target='blank'>";
-				        	
-							if( have_rows('footer_find_us_logo', 'option') ):
+					        if ($url)
+					        {
+					        	echo "<a href='".$url."' target='blank'>";
+					        	
+								if( have_rows('footer_find_us_logo', 'option') ):
 
-								// loop through the rows of data
-								while ( have_rows('footer_find_us_logo', 'option') ) : the_row();
+									// loop through the rows of data
+									while ( have_rows('footer_find_us_logo', 'option') ) : the_row();
 
-									if( get_row_layout() == 'html' ):
+										if( get_row_layout() == 'html' ):
 
-										the_sub_field('html');
+											the_sub_field('html');
 
-									elseif( get_row_layout() == 'image' ): 
+										elseif( get_row_layout() == 'image' ): 
 
-										error_log("Need to output images here");
+											error_log("Need to output images here");
 
-									endif;
+										endif;
 
-								endwhile;
+									endwhile;
 
-							endif;
+								endif;
 
-				        	echo "</a>";
-				        }
+					        	echo "</a>";
+					        }
 
-				    endwhile;
+					    endwhile;
 
-				else :
+					else :
 
-				    // no rows found
+					    // no rows found
 
-				endif;
+					endif;
 
-				?>
-			</p>
+					?>
+				</p>
 
+			</div>
 		</div>
 
 
 		<div class="cell small-12 medium-4 right-column">
 
-			<strong>A member of</strong>
-			<p class="a-member-of-address">
-				<?php $image = get_field('a_member_of', 'option');
+			<div class="container">
+				<strong>A member of</strong>
+				<p class="a-member-of-address">
+					<?php $image = get_field('a_member_of', 'option');
 
-				if( !empty($image) ): ?>
+					if( !empty($image) ): ?>
 
-					<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+						<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
 
-				<?php endif; ?>
-			</p>
+					<?php endif; ?>
+				</p>
+			</div>
 
 		</div>
 	</div><!-- .grid-x -->

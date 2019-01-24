49acde836c0426ea65c0521dc84ceb629a3afc6c
Steve <steve@australiansteve.com>
2019-01-23 22:11:09 -0400

    Reformatting ugly PHP/HTML mix

--- page-templates/about-page.php
+++ page-templates/about-page.php
@@ -48,103 +48,103 @@ get_header(); ?>
 
 			<div class="cell small-12" class="team">
 
-			<?php 
-				// check if the repeater field has rows of data
-				if( have_rows('team_members') ):
+<?php 
+	// check if the repeater field has rows of data
+	if( have_rows('team_members') ):
 
-					echo "<h2>Team</h2>";
+		echo "<h2>Team</h2>";
 
-				 	// loop through the rows of data
-				    while ( have_rows('team_members') ) : the_row();
-			?>
-						<div class="grid-x">
-							<div class="cell small-12 medium-3" class="team-member-image">
-								<?php
-								$image = get_sub_field('team_member_image');
+	 	// loop through the rows of data
+	    while ( have_rows('team_members') ) : the_row();
+?>
+				<div class="grid-x">
 
-								if( !empty($image) ): 
-									// vars
-									$alt = $image['alt'];
+					<div class="cell small-12 medium-3" class="team-member-image">
+<?php
+			$image = get_sub_field('team_member_image');
 
-									// thumbnail
-									$size = 'bio-pic-size';
-									$thumb = $image['sizes'][ $size ];
-									$width = $image['sizes'][ $size . '-width' ];
-									$height = $image['sizes'][ $size . '-height' ];
-								?>
-									 <img src="<?php echo $thumb; ?>" alt="<?php echo $alt; ?>" title="<?php the_sub_field('team_member_name'); ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>" />
-								 <?php
-								endif;
-								?>
-							</div>
-							<div class="cell small-12 medium-9" class="team-member-bio"><?php the_sub_field('team_member_bio'); ?></div>
-						</div>					        
-			<?php
-				    endwhile;
+			if( !empty($image) ): 
+				// vars
+				$alt = $image['alt'];
 
-				endif;
-			?>
+				// thumbnail
+				$size = 'bio-pic-size';
+				$thumb = $image['sizes'][ $size ];
+				$width = $image['sizes'][ $size . '-width' ];
+				$height = $image['sizes'][ $size . '-height' ];
+?>
+						<img src="<?php echo $thumb; ?>" alt="<?php echo $alt; ?>" title="<?php the_sub_field('team_member_name'); ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>" />
+ <?php
+			endif;
+?>
+					</div>
+					<div class="cell small-12 medium-9" class="team-member-bio"><?php the_sub_field('team_member_bio'); ?></div>
+				</div>					        
+<?php
+    	endwhile;
+
+	endif;
+?>
 
 			</div>
 
 			<div class="cell small-12" class="board">
 
-			<?php 
-				// check if the repeater field has rows of data
-				if( have_rows('board_of_directors') ):
+<?php 
+	// check if the repeater field has rows of data
+	if( have_rows('board_of_directors') ):
 
-					echo "<h2>Board of Directors</h2>";
-					$b = 0;
-				?>
-					<div class="grid-x">
-				<?php
-				 	// loop through the rows of data
-				    while ( have_rows('board_of_directors') ) : the_row();
-			?>
-							<div class="cell small-4 medium-2 bod-image <?php echo ($b == 0) ? 'active' : ''?>">
-								<?php
-								$image = get_sub_field('bod_image');
+		echo "<h2>Board of Directors</h2>";
+		$b = 0;
+?>
+				<div class="grid-x">
+<?php
+		// loop through the rows of data
+		while ( have_rows('board_of_directors') ) : the_row();
+?>
 
-								if( !empty($image) ): 
-									// vars
-									$alt = $image['alt'];
+					<div class="cell small-4 medium-2 bod-image <?php echo ($b == 0) ? 'active' : ''?>">
 
-									// thumbnail
-									$size = 'thumbnail';
-									$thumb = $image['sizes'][ $size ];
-									$width = $image['sizes'][ $size . '-width' ];
-									$height = $image['sizes'][ $size . '-height' ];
-								?>
-									 <img src="<?php echo $thumb; ?>" alt="<?php echo $alt; ?>" title="<?php the_sub_field('bod_name'); ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>" data-id="<?php echo $b;?>"/>
-	 								<div class="hidden bod-bio" style="display:none" data-id="<?php echo $b;?>"><?php the_sub_field('bod_bio'); ?></div>
+<?php
+			$image = get_sub_field('bod_image');
+
+			if( !empty($image) ): 
+				// vars
+				$alt = $image['alt'];
+
+				// thumbnail
+				$size = 'thumbnail';
+				$thumb = $image['sizes'][ $size ];
+				$width = $image['sizes'][ $size . '-width' ];
+				$height = $image['sizes'][ $size . '-height' ];
+?>
+						<img src="<?php echo $thumb; ?>" alt="<?php echo $alt; ?>" title="<?php the_sub_field('bod_name'); ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>" data-id="<?php echo $b;?>"/>
+						<div class="hidden bod-bio" style="display:none" data-id="<?php echo $b;?>"><?php the_sub_field('bod_bio'); ?></div>
 	 								
-								 <?php
-								endif;
-								?>
-							</div>		
-			<?php
-						$b++;
+<?php
+			endif;
+?>
+					</div>		
+<?php
+			$b++;
 
-						if ($b % 3 == 0 || $b == count(get_field('board_of_directors')))
-						{
-							echo "<div class='bio-display cell small-12 hide-for-medium'></div>";
-						}
-						if ($b % 6 == 0 || $b == count(get_field('board_of_directors')))
-						{
-							echo "<div class='bio-display cell small-12 show-for-medium'></div>";
-						}
-				    endwhile;
-				    ?>
-						</div>
-						<!-- The following divs will be used if there's an uneven number of board members displayed -->
+			if ($b % 3 == 0 || $b == count(get_field('board_of_directors')))
+			{
+				echo "<div class='bio-display cell small-12 hide-for-medium'></div>";
+			}
+			if ($b % 6 == 0 || $b == count(get_field('board_of_directors')))
+			{
+				echo "<div class='bio-display cell small-12 show-for-medium'></div>";
+			}
+		endwhile;
+?>
+				</div>
 						
-					<?php
-
-				endif;
-			?>
+<?php
+	endif;
+?>
 
 			</div>
-
 
 		<?php endwhile; // End of the loop. ?>
 

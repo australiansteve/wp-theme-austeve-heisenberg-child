797c1f2a7c4f271b3f22a978655f4d35232cecaf
Steve <steve@australiansteve.com>
2019-03-05 21:54:40 +1100

    Update search form to direct to funds page

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
-<form action="/" method="get" class="search-form">
+<form action="<?php echo site_url('/funds/');?>" method="get" class="search-form">
 	<div class="grid-x align-right">
 		<div class="cell shrink">
-		    <input type="text" name="s" id="search" value="<?php the_search_query(); ?>" />
+		    <input type="text" name="fund-name" id="search" value="<?php the_search_query(); ?>" />
 		</div>
 		<div class="cell small-2">
 		    <input type="submit" value="go" id="submit"></input>

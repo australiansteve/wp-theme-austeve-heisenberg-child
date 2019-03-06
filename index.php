8cf9c147de82f994feb75cf8686fa068b38e1736
Steve <steve@australiansteve.com>
2019-03-06 21:16:42 +1100

    Add basic index template file to child

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+get_header(); ?>
+
+	<div class="grid-x">
+
+		<main class="cell small-12">
+			<?php
+			if ( have_posts() ) :
+
+				while ( have_posts() ) :
+
+					the_post();
+
+					the_title( '<h1>', '</h1>' );
+
+					the_content();
+
+				endwhile;
+
+				the_posts_navigation();
+
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			else :
+
+				echo esc_html( 'Sorry, no posts' );
+
+			endif; ?>
+
+		</main>
+
+	</div>
+
+<?php
+get_footer();

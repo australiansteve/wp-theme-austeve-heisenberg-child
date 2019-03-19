aad5d4cdf86f3ab468ade77cf346fcdb541031ef
Steve <steve@australiansteve.com>
2019-03-19 10:02:18 -0300

    Add template part for Featured Funds and add 2 to the homepage

---
+++ template-parts/austeve-funds-featured.php
@@ -0,0 +1,30 @@
+<?php
+$bgColor = get_field('background_color');
+if(!$bgColor)
+{
+	$bgColor = '#bc5298';
+}
+
+?>
+<div class="cell small-12 medium-6 featured-fund">
+	<div class="container">
+		<div class="bg-image" style="background-image:url(<?php the_post_thumbnail_url();?>)"></div>
+		<div class="bg-color" style="background-color:<?php echo $bgColor; ?>"></div>
+
+		<div class="grid-x align-middle featured-fund">
+			<div class="cell small-12">
+
+				<?php the_title('<h3>', '</h3>'); ?>
+				
+				<div class="description">
+					<?php the_field('short_description'); ?>
+				</div>
+
+				<div class="action">		
+					<a class="button" href="<?php the_permalink();?>">DONATE NOW</a>
+				</div>
+
+			</div>
+		</div>
+	</div>
+</div>

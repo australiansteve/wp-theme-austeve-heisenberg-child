d4bd29ed9da3f3a0b98a8a5f53d9f6299e7fb341
Steve <steve@australiansteve.com>
2019-04-09 08:31:26 -0300

    404 page template

---
+++ 404.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found).
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package Heisenberg
+ */
+
+get_header(); ?>
+
+	<div class="grid-x">
+
+		<main class="cell">
+
+			<h1>Oops! That page can't be found.</h1>
+
+			<p>It looks like nothing was found at this location.</p>
+
+			<p>Try the <a href="/">home page</a>, or <a href="/contact">contact us</a> if you think this is a problem.
+
+		</main>
+
+	</div>
+
+<?php
+get_footer();

dc0443a377f00f5bdd316f1dd5607f8e6f2204b9
Steve <steve@australiansteve.com>
2019-04-11 20:33:54 -0300

    Add section titles on home page and link to stories

--- front-page.php
+++ front-page.php
@@ -30,36 +30,47 @@ if ( have_posts() ) :
 	if(!$bgColor):
 		$bgColor = '#7fb955';
 	endif;
-	?>
+
+	$featuredPostsArgs = array(
+		'post_type'				=> array( 'post' ),
+		'post_status'           => array( 'publish' ),
+		'tax_query'				=> array(
+			'relation' => 'AND',
+			array(
+				'taxonomy'         => 'category',
+				'terms'            => 'featured',
+				'field'            => 'slug',
+				'operator'         => 'IN',
+				'include_children' => false,
+			),
+		),
+		'posts_per_page'		=> 1
+	);
+
+	// The Query
+	$featuredFPostsQuery = new WP_Query( $featuredPostsArgs );
+	if ( $featuredFPostsQuery->have_posts() ) :
+?>
+	<h2 class="title featured-post">Featured Story</h2>	
 	<div class="grid-x align-middle" id="featured-post" style="background-color:<?php echo $bgColor;?>">
 <?php
+		while ( $featuredFPostsQuery->have_posts() ) : 
+			$featuredFPostsQuery->the_post();
+			get_template_part( 'template-parts/post', 'featured' );
+		endwhile; 
+?>
+	</div>
+<?php
+	endif;
+	wp_reset_postdata();
+	?>	
 
-$featuredPostsArgs = array(
-	'post_type'				=> array( 'post' ),
-	'post_status'           => array( 'publish' ),
-	'tax_query'				=> array(
-		'relation' => 'AND',
-		array(
-			'taxonomy'         => 'category',
-			'terms'            => 'featured',
-			'field'            => 'slug',
-			'operator'         => 'IN',
-			'include_children' => false,
-		),
-	),
-	'posts_per_page'		=> 1
-);
-
-// The Query
-$featuredFPostsQuery = new WP_Query( $featuredPostsArgs );
-if ( $featuredFPostsQuery->have_posts() ) :
-	while ( $featuredFPostsQuery->have_posts() ) : 
-		$featuredFPostsQuery->the_post();
-		get_template_part( 'template-parts/post', 'featured' );
-	endwhile; 
-endif;
-wp_reset_postdata();
-?>	
+	<div class="grid-x" id="more-stories">
+		<div class="cell">
+			<div class="action">		
+				<a class="button" href="<?php echo get_permalink(get_option('page_for_posts')); ?>"><?php the_field('more_stories_button_text')?></a>
+			</div>
+		</div>
 	</div>
 
 	<div class="grid-x" id="what-is-cf">

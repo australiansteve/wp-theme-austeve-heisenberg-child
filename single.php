85cf4609b4014dc9e5199c6d99b1ee286a024423
Steve <steve@australiansteve.com>
2019-04-11 23:30:08 -0300

    Single post template complete

--- single.php
+++ single.php
@@ -9,39 +9,70 @@ if ( have_posts() ) :
 
 	while ( have_posts() ) : 
 		the_post();
-
+?>
+			<div id="single-post-content">
+<?php
 		the_title( '<h1>', '</h1>' );
 
-		the_content();
+		if (has_post_thumbnail()):
+			the_post_thumbnail('feature-pic-size');
+		endif;
 
+		the_content();
 ?>
+			</div>
+<?php
+		$bgColor = array('#bc5298', '#7fb955', '#6abfdb'); //pink, green, blue
+?>
+			<h2>More stories</h2>
 			<div class="grid-x align-center" id="story-navigation">
 <?php
 			if( $prev_post = get_previous_post() ): 
 ?>
-				<div class="cell small-12 medium-4 prev-post">
+				<div class="cell small-12 medium-4 post-nav">
+					<div class='container align-middle'>
+						<div class="bg-image" style="background-image:url(<?php echo get_the_post_thumbnail_url($prev_post->ID);?>)"></div>
+						<div class="bg-color" style="background-color:<?php echo $bgColor[0]; ?>"></div>
+						<div class="grid-x align-center-middle content">
+							<div class="cell">
 <?php 
-				$prevpost = get_the_post_thumbnail( $prev_post->ID, 'medium', array('class' => 'pagination-previous')); 
-				previous_post_link( '%link',"$prevpost  <h3>%title</h3>" ); 
-?>
+				previous_post_link( '%link',"<h3>%title</h3>" ); 
+?>						
+							</div>
+						</div>
+					</div>
 				</div>
 <?php
 			endif; 
 
 			if( $next_post = get_next_post() ): 
+				error_log("Next post:".$next_post->ID);
 ?>
-				<div class="cell small-12 medium-4 next-post">
+				<div class="cell small-12 medium-4 post-nav">
+					<div class='container align-middle'>
+						<div class="bg-image" style="background-image:url(<?php echo get_the_post_thumbnail_url($next_post->ID);?>)"></div>
+						<div class="bg-color" style="background-color:<?php echo $bgColor[1]; ?>"></div>
+						<div class="grid-x align-center-middle content">
+							<div class="cell">
 <?php 
-				$nextpost = get_the_post_thumbnail( $next_post->ID, 'medium', array('class' => 'pagination-next')); 
-				next_post_link( '%link',"$nextpost  <h3>%title</h3>" ); 
-?>
+				next_post_link( '%link',"<h3>%title</h3>" ); 
+?>						
+							</div>
+						</div>
+					</div>
 				</div>
 <?php
 			endif; 
 ?>
-				<div class="cell small-12 medium-4">
-					<div class="container">
-						<a class="button" href="/stories">All stories</a>
+				<div class="cell small-12 medium-4 post-nav">
+					<div class='container align-middle'>
+						<div class="bg-image" style="background-image:url(<?php the_post_thumbnail_url();?>)"></div>
+						<div class="bg-color" style="background-color:<?php echo $bgColor[2]; ?>"></div>
+						<div class="grid-x align-center-middle content">
+							<div class="cell">
+								<a href="/stories"><h3><?php the_field('more_stories_button_text', get_option( 'page_on_front' ))?></h3></a>						
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>

a28078b44a14e386f119b1932e56da67d18d4ca5
Steve <steve@australiansteve.com>
2020-05-06 08:49:31 -0300

    Add popup dialog with options set in backend

--- footer.php
+++ footer.php
@@ -10,65 +10,84 @@
  */
 ?>
 
-		</div><!-- #content -->
-		
-	</div><!-- .off-canvas-wrapper -->
+					<?php 
+					if (get_field('display_popup_modal_dialog', 'option') && is_front_page()) :
+						get_template_part('template-parts/modal'); ?>
+						<script type="text/javascript">
 
-</div><!-- #page -->
+							jQuery(document).ready(function($) {
+								setTimeout(function() {
+									jQuery('#popup-modal').foundation('open');
+								}, <?php the_field('modal_display_after_milliseconds', 'option') ?>);
+							});
 
-<footer id="colophon" class="site-footer">
-	<div class="grid-x grid-margin-x">
+							jQuery("#popup-modal").on('open.zf.reveal', function($) {
+								console.log("Change opacity");
+								jQuery(this).css("opacity", "1");
+								jQuery(".reveal-overlay").css("opacity", "1");
 
-		<div class="cell small-12 medium-3 medium-text-left footer-contact">
+							});
+						</script>
+					<?php endif; ?>
 
-			<h3>Contact</h3>
+					</div><!-- #content -->
 
-			<p class="footer-address">
-				<?php the_field('address', 'option'); ?>
-			</p>				
-			<!--
-			<p class="footer-signup">
-				<a href="mailchimp.com" target="_blank">sign up for our newsletter</a>
-			</p>
-			-->
-			<p class="footer-social">
-				<a href="https://www.instagram.com/eastcoastbistr0" target="_blank"><i class="fab fa-instagram fa-2x"></i></a><a href="https://www.facebook.com/EastCoastBistro" target="_blank"><i class="fab fa-facebook fa-2x"></i></a>
-			</p>
+				</div><!-- .off-canvas-wrapper -->
 
-		</div>
+			</div><!-- #page -->
 
-		<div class="cell small-12 medium-4 medium-text-left footer-hours">
-			
-			<h3>Hours</h3>
-			<?php 
-			// check if the repeater field has rows of data
-			if( have_rows('hours', 'option') ):
+			<footer id="colophon" class="site-footer">
+			<div class="grid-x grid-margin-x">
 
-			 	// loop through the rows of data
-			    while ( have_rows('hours', 'option') ) : the_row();
-			    ?>
-			    	<div class="grid-x grid-margin-x">
-			    		<div class="cell auto day text-left"><?php the_sub_field('day'); ?><span class="filler"></span></div>
-			    		<div class="cell shrink times"><?php the_sub_field('times'); ?></div>
-			    	</div>
-		       <?php
-			    endwhile;
+				<div class="cell small-12 medium-3 medium-text-left footer-contact">
 
-			endif;
-			?>
-		</div>	
+					<h3>Contact</h3>
 
-		<div class="cell small-12 medium-5 medium-text-right">
+					<p class="footer-address">
+						<?php the_field('address', 'option'); ?>
+					</p>				
+					<!--
+					<p class="footer-signup">
+						<a href="mailchimp.com" target="_blank">sign up for our newsletter</a>
+					</p>
+					-->
+					<p class="footer-social">
+						<a href="https://www.instagram.com/eastcoastbistr0" target="_blank"><i class="fab fa-instagram fa-2x"></i></a><a href="https://www.facebook.com/EastCoastBistro" target="_blank"><i class="fab fa-facebook fa-2x"></i></a>
+					</p>
 
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/_dist/img/ECB-logo-all-gold.png" alt-text="East Coast Bistro"/>
+				</div>
 
-		</div>	
+				<div class="cell small-12 medium-4 medium-text-left footer-hours">
 
-		
+					<h3>Hours</h3>
+					<?php 
+						// check if the repeater field has rows of data
+					if( have_rows('hours', 'option') ):
 
-	</div>
-</footer><!-- #colophon -->
+						 	// loop through the rows of data
+						while ( have_rows('hours', 'option') ) : the_row();
+							?>
+							<div class="grid-x grid-margin-x">
+								<div class="cell auto day text-left"><?php the_sub_field('day'); ?><span class="filler"></span></div>
+								<div class="cell shrink times"><?php the_sub_field('times'); ?></div>
+							</div>
+							<?php
+						endwhile;
 
-<?php wp_footer(); ?>
-</body>
+					endif;
+					?>
+				</div>	
+
+				<div class="cell small-12 medium-5 medium-text-right">
+
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/_dist/img/ECB-logo-all-gold.png" alt-text="East Coast Bistro"/>
+
+				</div>	
+
+			</div>
+
+		</footer><!-- #colophon -->
+
+		<?php wp_footer(); ?>
+	</body>
 </html>
---
+++ template-parts/modal.php
@@ -0,0 +1,8 @@
+<div class="reveal text-center" id="popup-modal" data-reveal style="opacity:0">
+  <h2><?php the_field('modal_title', 'option') ?></h2>
+  <?php the_field('modal_text', 'option') ?>
+  	<button class="close-button" data-close aria-label="Close modal" type="button">
+  <span aria-hidden="true">&times;</span>
+</button>
+
+</div>
